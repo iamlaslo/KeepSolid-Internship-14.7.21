db7edb6d146a01dd92aa364a173e809b96fd1d7b
yavladikku <yavladikku@gmail.com>
2021-07-14 15:33:46 +0300

    Initial Commit

---
+++ KeepSolid-Internship-14.7.21/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KeepSolid-Internship-14.7.21
+//
+//  Created by Vlad Kozlov on 14.7.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
